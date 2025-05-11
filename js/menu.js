b7e5b1bfc761c522851cfa2ef41f86879d92b5bd
GuilhermeSaraivaL <Guilherme.A.Saraiva.L@gmail.com>
2025-05-11 04:23:52 -0300

    Refactor styles and HTML structure for improved organization and functionality; add smooth scrolling and contact form

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ let menuButton = document.getElementById('btn-abrir-menu-mobile');
 let menu = document.getElementById('menu-mobile');
 let closeButton = document.getElementById('btn-fechar-menu-mobile');
 let overlay = document.getElementById('overlay-menu-mobile');
+let site = document.getElementById('btn-inicio-site');
 
 menuButton.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
@@ -18,4 +19,8 @@ closeButton.addEventListener('click', ()=>{
 
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
+})
+
+site.addEventListener('click', ()=>{
+    window.scrollTo({top: 0, behavior: 'smooth'});
 })
